7773f3af19dbdbac6da4ba24ae27e9d24953b8c6
wwjddbss126 <forjy4815@gmail.com>
2020-10-07 21:56:44 +0900

    FoJ_G_마음마켓

---
+++ G조 - 마음마켓/app/src/main/java/com/example/foj_project/data/model/LoggedInUser.kt
@@ -0,0 +1,8 @@
+package com.example.foj_project.data.model
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
